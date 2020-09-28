72d02f4170e8fd579096ebe508781ba7da921de7
ActiveOne/DanielMarín <daniel.marin@activeone.com>
2020-09-28 14:01:15 -0500

    Add a CRUD for employees

---
+++ src/components/EmployeeButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>{{title.name}}</button>
+</template>
+
+<script>
+export default {
+  name: 'employee-button',
+  props: ['title']
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/data/employees.js
@@ -0,0 +1,24 @@
+const employees = [
+  {
+    id: 1,
+    name: 'Diego Marquez',
+    email: 'diego@mail.com'
+  },
+  {
+    id: 2,
+    name: 'Pepito Pérez',
+    email: 'pepito@mail.com'
+  },
+  {
+    id: 3,
+    name: 'Oriana Cogollo',
+    email: 'oriana@mail.com'
+  },
+  {
+    id: 4,
+    name: 'Daniel Marín',
+    email: 'daniel@mail.com'
+  },
+]
+
+export default employees;
